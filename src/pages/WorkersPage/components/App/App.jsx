7f67ba562d07b4bf2dc3e2ba1171d97a0e53e2da
Yurii04UA <uyriparaka@gmail.com>
2022-05-02 18:27:16 +0300

    react

--- src/pages/WorkersPage/components/App/App.jsx
+++ src/pages/WorkersPage/components/App/App.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import AppFilter from '../AppFilter/AppFilter';
-import AppInfo from '../AppInfo/AppInfo';
-import SearchPanel from '../SearchPanel/SearchPanel';
-import EmploeersAddForm from '../EmploeersAddForm/EmploeersAddForm';
-import EmploeersList from '../EmploeersList/EmploeersList';
-import EmploeersListItem from '../EmploeersListItem/EmploeersListItem';
+import React from "react";
+import AppFilter from "../AppFilter/AppFilter";
+import AppInfo from "../AppInfo/AppInfo";
+import SearchPanel from "../SearchPanel/SearchPanel";
+import EmploeersAddForm from "../EmploeersAddForm/EmploeersAddForm";
+import EmploeersList from "../EmploeersList/EmploeersList";
 
-import "./App.css"
+import "./App.css";
 
 const App = () => {
+  const data = [
+    { name: "Jonn C", salary: 800 ,increase: false, id: 1},
+    { name: "Par L", salary: 1000 ,increase: true, id: 2},
+    { name: "Mar S", salary: 1100 ,increase: false, id: 3},
+    { name: "Had A", salary: 950 ,increase: false, id: 4},
+  ];
+
   return (
-    <div className='app'>
+    <div className="app">
       <AppInfo />
 
       <div className="search_panel">
         <SearchPanel />
         <AppFilter />
       </div>
-      <EmploeersList />
-      
-      <EmploeersAddForm />
-      
-    
-    </div>
-  )
-}
+      <EmploeersList data={data}/>
 
-export default App
+      <EmploeersAddForm />
+    </div>
+  );
+};
+
+export default App;
--- src/pages/WorkersPage/components/EmploeersList/EmploeersList.jsx
+++ src/pages/WorkersPage/components/EmploeersList/EmploeersList.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./EmploeersList.css";
-import EmploeersListItem from "../EmploeersListItem/EmploeersListItem"
+import EmploeersListItem from "../EmploeersListItem/EmploeersListItem";
 
-const EmploeersList = () => {
-  return <ul className="app-list list-group">
-            <EmploeersListItem />
-            <EmploeersListItem />
-            <EmploeersListItem />
-         </ul>
+const EmploeersList = ({ data }) => {
+
+  const elemEmployees = data.map((item)=>{
+    
+    return (
+      <EmploeersListItem name={item.name} salary={item.salary} key={item.id} increase={item.increase} />  /// можно написать {...item} просто развернет весь обьект
+    );
+  })
+  return (
+    <ul className="app-list list-group">
+      {elemEmployees}
+    </ul>
+  );
 };
 
 export default EmploeersList;
