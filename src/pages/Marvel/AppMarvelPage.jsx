37417b795318476e19bf004e8c1047edfad14f2c
Yurii04UA <uyriparaka@gmail.com>
2022-05-14 20:25:48 +0300

    reacr

--- src/pages/Marvel/AppMarvelPage.jsx
+++ src/pages/Marvel/AppMarvelPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './style/style.scss'
-import App from './components/app/App'
+import React from "react";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+
+import "./style/style.scss";
+
+const marvelService = new MarvelService();
+// marvelService.getAllCharacters().then(res => console.log(res.data.results))
+marvelService.getAllCharacters().then((res) => res.data.results.forEach(item => console.log(item.name)))
 
 const AppMarvelPage = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <App />
+      {/* <MarvelService /> */}
     </div>
-  )
-}
+  );
+};
 
-export default AppMarvelPage
+export default AppMarvelPage;
