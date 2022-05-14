e64097efa7b12db9521ae1803ffedba16888d129
Yurii04UA <uyriparaka@gmail.com>
2022-05-14 23:16:57 +0300

    react

--- src/pages/Marvel/AppMarvelPage.jsx
+++ src/pages/Marvel/AppMarvelPage.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
-// marvelService.getAllCharacters().then(res => console.log(res.data.results))
-marvelService.getAllCharacters().then((res) => res.data.results.forEach(item => console.log(item.name)))
 
 const AppMarvelPage = () => {
   return (
     <div className="container">
       <App />
-      {/* <MarvelService /> */}
     </div>
   );
 };
---
+++ src/pages/Marvel/services/MarvelService.js
@@ -0,0 +1,43 @@
+class MarvelService {
+  
+  _apiBase = `https://gateway.marvel.com:443/v1/public/`;
+  _apiKey = `apikey=def8ee68493ef8fbd835548a5db3f260`;
+
+  getResurse = async (url) => {
+    let result = await fetch(url);
+
+    if (!result.ok) {
+      throw new Error(`Could not fetch ${url}, status ${result.status}`);
+    }
+
+    return await result.json();
+  };
+
+  getAllCharacters = async () => {
+    const result = await this.getResurse(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+    return result.data.results.map(this._transformCharacter);
+  };
+  getCharacter = async (id) => {
+    const result = await this.getResurse(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(result.data.results[0]);
+  };
+
+  _transformCharacter = (res) => {
+    return {
+      name: res.name,
+      description: res.description,
+      thumbnail:
+        res.thumbnail.path +
+        "." +
+        res.thumbnail.extension,
+      homepage: res.urls[0].url,
+      wiki: res.urls[1].url,
+    };
+  };
+}
+
+export default MarvelService;
