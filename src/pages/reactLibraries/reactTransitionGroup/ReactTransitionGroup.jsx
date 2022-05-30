7d53e6cc56fa7d494d70dc242ca68eac23c4da50
Yurii04UA <uyriparaka@gmail.com>
2022-05-30 15:51:11 +0300

    react libraris

--- src/pages/reactLibraries/reactTransitionGroup/ReactTransitionGroup.jsx
+++ src/pages/reactLibraries/reactTransitionGroup/ReactTransitionGroup.jsx
@@ -1,14 +1,94 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Transition } from "react-transition-group";
+
+import "./ReactTransitionGroup.css";
+
+const Modal = (props) => {
+  const duration = 300;
+
+  const defaultStyle = {
+    transition: `all ${duration}ms ease-in-out`,
+    opacity: 0,
+    visibility: 'hidden'
+  };
+
+  const transitionStyles = {
+    entering: { opacity: 1, visibility: 'visible',transform: 'translate(0%)' },
+    entered: { opacity: 1, visibility: 'visible',transform: 'translate(0%)' },
+    exiting: { opacity: 0, visibility: 'hidden',transform: 'translate(100%)'  },
+    exited: { opacity: 0, visibility: 'hidden',transform: 'translate(100%)' },
+  };
+
+  return (
+    <Transition timeout={duration} in={props.show}>
+      {(state) => (
+        <div className="modal mt-5 d-block " style={{
+         ...defaultStyle,
+         ...transitionStyles[state]
+       }}>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Typical modal window</h5>
+                <button
+                  onClick={() => props.onClose(false)}
+                  type="button"
+                  className="btn-close"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <p>Modal body content</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  onClick={() => props.onClose(false)}
+                  type="button"
+                  className="btn btn-secondary"
+                >
+                  Close
+                </button>
+                <button
+                  onClick={() => props.onClose(false)}
+                  type="button"
+                  className="btn btn-primary"
+                >
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </Transition>
+  );
+};
 
 const ReactTransitionGroup = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>
-       <NavLink className='link container mb-3 w-75' to='/react-libraries'> ← Back</NavLink>
-
+    <>
+      <nav>
+        <NavLink className="link container mb-3 w-75" to="/react-libraries">
+          ← Back
+        </NavLink>
+      </nav>
+      <Container>
+        <Modal onClose={setShowModal} show={showModal} />
+        <button
+          className="btn btn-warning btn-my mt-5"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          Open modal
+        </button>
        
-    </div>
-  )
-}
+      </Container>
+    </>
+  );
+};
 
-export default ReactTransitionGroup
+export default ReactTransitionGroup;
