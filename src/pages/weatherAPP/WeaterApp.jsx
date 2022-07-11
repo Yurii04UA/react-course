c9c75364254af8263ce07dd7e6b92b01fcc776f5
Yurii04UA <uyriparaka@gmail.com>
2022-07-11 14:25:15 +0300

    basic structure

--- src/pages/weatherAPP/WeaterApp.jsx
+++ src/pages/weatherAPP/WeaterApp.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Home from './Pages/Home/Home'
+import "./style/index.scss"
 
 const WeaterApp = () => {
   return (
-    <div>weater App</div>
+    <div className='weatherApp'>
+      weater App
+      <Home />
+      </div>
   )
 }
 
