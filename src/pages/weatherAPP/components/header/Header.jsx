bc73a6b3c3f98cffb049cec6c9b060f789601301
Yurii04UA <uyriparaka@gmail.com>
2022-07-15 00:30:22 +0300

    someshit for theme

--- src/pages/weatherAPP/components/header/Header.jsx
+++ src/pages/weatherAPP/components/header/Header.jsx
@@ -8,28 +8,28 @@ import logo from "./logo.png";
 import icon from "./switch_theme.png";
 
 const Header = () => {
-  const themeGlobal = useContext(ThemeContext)
+  const themeGlobal = useContext(ThemeContext);
   // const [thema, setThema] = useState("light");
   const changeTheme = () => {
     // setThema(thema === "light" ? "dark" : "light");
     // themeGlobal.changeTheme(themeGlobal.theme === 'light'? "dark" : "light")
-      themeGlobal.changeTheme()    
+    themeGlobal.changeTheme();
   };
 
   useEffect(() => {
     const root = document.querySelector(":root");
     const components = [
-      '--components-backround-',
-      '--card-backround-',
-      '--text-color-',
-      '--shadow-color-',
-    ]
-    components.forEach(elem =>{
+      "--components-backround-",
+      "--card-backround-",
+      "--text-color-",
+      "--shadow-color-",
+    ];
+    components.forEach((elem) => {
       root.style.setProperty(
         `${elem}default`,
         `var(${elem}${themeGlobal.theme})`
       );
-    })
+    });
   }, [themeGlobal.theme]);
   return (
     <div className="header">
@@ -38,7 +38,10 @@ const Header = () => {
         <h2>Weather app</h2>
       </div>
       <div className="wrapper">
-        <img src={icon} alt="icon_select_theme" onClick={changeTheme} />
+        <div className="img" onClick={changeTheme}>
+          <p>Switch theme</p>
+          <img src={icon} alt="icon_select_theme"  />
+        </div>
         <select className="select">
           <option value="select">Select city</option>
           <option value="location">Your location</option>
