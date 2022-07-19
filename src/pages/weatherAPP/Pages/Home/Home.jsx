13654b226349bbf369b2a0c94486b6a9b2dff845
Yurii04UA <uyriparaka@gmail.com>
2022-07-19 16:53:05 +0300

    weather app

--- src/pages/weatherAPP/Pages/Home/Home.jsx
+++ src/pages/weatherAPP/Pages/Home/Home.jsx
@@ -2,36 +2,40 @@ import React, { useEffect } from "react";
 import ThisDay from "./components/ThisDay/ThisDay";
 import ThisDayInfo from "./components/ThisDayInfo/ThisDayInfo";
 import Days from "./components/Days/Days";
-import Popup from "../../components/Popup/Popup"
+import Popup from "../../components/Popup/Popup";
 import { fetchCurrentWeather } from "../../store/slices/thunks/fetchCurrentWeather";
 import "./Home.scss";
 import { NavLink } from "react-router-dom";
 import Header from "../../components/header/Header";
 import { useState } from "react";
 
-
 const Home = () => {
-  const [data,setData] = useState([]);
-  const [city,setCity] = useState('dnipro')
-  useEffect(()=>{
-   
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3a14ed878c17ea88667c02de9b9be534`).then(res => res.json()).then(data => setData(data))    
-      // console.log(1658236389);
-      // console.log(Date.now());
-  },[city])
+  const [data, setData] = useState([]);
+  const [city, setCity] = useState("dnipro");
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3a14ed878c17ea88667c02de9b9be534`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data)).then(()=> setIsLoading(false));
+    ;
+    // console.log(1658236389);
+    // console.log(Date.now());
+  }, [city]);
   return (
-    
-      <div className="container-weather">
-        <NavLink to="additional-statistics">sd</NavLink>
-        <Header />
-        <div className="thisDayWrapper">
-          <ThisDay />
-          <ThisDayInfo />
-        </div>
-
-        <Days />
-        {/* <Popup /> */}
+    <div className="container-weather">
+      <NavLink to="additional-statistics">sd</NavLink>
+      <Header />
+      <div className="thisDayWrapper">
+        <ThisDay data={data} loading={isLoading} />
+        <ThisDayInfo />
       </div>
+
+      <Days />
+      {/* <Popup /> */}
+    </div>
   );
 };
 
--- src/pages/weatherAPP/Pages/Home/components/ThisDay/ThisDay.jsx
+++ src/pages/weatherAPP/Pages/Home/components/ThisDay/ThisDay.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import "./ThisDay.scss";
 import sun from "./images/sun.png";
 
-const ThisDay = () => {
+const ThisDay = ({data,loading}) => {
+  const {name,main,weather} = data
   const time = `${new Date().getHours()}:${new Date().getMinutes()}`
+
   return (
     <div className="thisDay">
       <div className="box">
         <div className="info">
-          <h2>20°</h2>
+        {loading? <h2>--</h2>:  <h2>{(main.temp).toFixed(0)}°</h2>}
           <p>Today</p>
         </div>
-        <img src={sun} alt="Sun" />
+        {loading? <img src="smt-img" alt='loading' /> : <img src="smt-img" alt={weather[0].description} />}
       </div>
       <p>Time: <span>{time}</span></p>
-      <p>City: <span>Dnipro</span></p>
+      <p>City: <span>{name}</span></p>
     </div>
   );
 };
