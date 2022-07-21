591c67c43c88133ce1551fb48024befd280297a7
Yurii04UA <uyriparaka@gmail.com>
2022-07-21 16:45:44 +0300

    added icons for day

--- src/pages/weatherAPP/Pages/Home/components/ThisDay/ThisDay.jsx
+++ src/pages/weatherAPP/Pages/Home/components/ThisDay/ThisDay.jsx
@@ -5,7 +5,7 @@ import sun from "./images/sun.png";
 const ThisDay = ({data,loading}) => {
   const {name,main,weather} = data
   const time = `${new Date().getHours()}:${new Date().getMinutes()}`
-
+ 
   return (
     <div className="thisDay">
       <div className="box">
@@ -13,7 +13,9 @@ const ThisDay = ({data,loading}) => {
         {loading? <h2>--</h2>:  <h2>{(main.temp).toFixed(0)}°</h2>}
           <p>Today</p>
         </div>
-        {loading? <img src="smt-img" alt='loading' /> : <img src="smt-img" alt={weather[0].description} />}
+        <div className="weather-box">
+        {loading? <img src="smt-img" alt='loading' /> : <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt={weather[0].description} />}
+        </div>
       </div>
       <p>Time: <span>{time}</span></p>
       <p>City: <span>{loading? <span>--</span>: name}</span></p>
